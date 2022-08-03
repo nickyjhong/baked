ee8140e7697d1e3686bb0c8e368f5dabf287cd2e
Chris Tomshack <ctomshack15@gmail.com>
2022-08-03 15:46:00 -0400

    API: added product routes

---
+++ server/api/products.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = require('express').Router()
+const Product  = require('../db/models/product');
+
+// USER VIEW: VIEW ALL PRODUCTS
+router.get('/shop', async (req, res, next) => {
+    try {
+      const products = await Product.findAll();
+  
+      res.send(products)
+    } catch (err) {
+      next(err) 
+    }
+  })
+  
+//   USER VIEW: VIEW A SINGLE PRODUCT
+  router.get('/shop/:id', async (req, res, next) => {
+    try {
+      const products = await Product.findByPk(req.params.id)
+      res.json(products)
+    } catch (err) {
+      next(err)
+    }
+  })
+  
+//   ADMIN VIEW: ADD A NEW PRODUCT TO SHOP PAGE
+  router.post('/shop', async (req, res, next) => {
+    try {
+      res.status(201).send(await Product.create(req.body));
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  //   ADMIN VIEW: UPDATE A NEW PRODUCT TO SHOP PAGE
+  router.put('/shop/:id', async (req, res, next) => {
+    try {
+      const product = await Product.findByPk(req.params.id);
+      res.send(await product.update(req.body));
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+//   ADMIN VIEW: DELETE A PRODUCT FROM SHOP
+  router.delete('/:id', async (req, res, next) => {
+    try {
+      const product = await Product.findByPk(req.params.id);
+      await product.destroy();
+      res.send(product);
+    } catch (error) {
+      next(error);
+    }
+  });
+  
+  module.exports = router
