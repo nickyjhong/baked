ba35d2b25818c5518a9bceac2e0e2aeb6af1f570
Cherry Xu <ms.cherry.xu@gmail.com>
2022-08-05 00:06:05 -0400

    Filtered clicks on categories, added api route for categories, and added fetch category type

--- client/store/allProducts.js
+++ client/store/allProducts.js
@@ -1,56 +1,88 @@
-import axios from "axios";
+import axios from 'axios';
 
 // Action type
-const SET_PRODUCTS = "SET_PRODUCTS";
+const SET_PRODUCTS = 'SET_PRODUCTS';
 const CREATE_PRODUCT = 'CREATE_PRODUCT';
 // const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 const DELETE_PRODUCT = 'DELETE_PRODUCT';
+const FETCH_PRODUCT_TYPE = 'FETCH_PRODUCT_TYPE';
 
 // Action creator
-const _createProduct = product => {
-  return {
-    type: CREATE_PRODUCT,
-    product
-  }
-}
 
-
-const _deleteProduct = product => {
-  return {
-    type: DELETE_PRODUCT,
-    product
-  }
-}
-
-
-// THUNK CREATORS
-export const createProduct = (product, history) => {
-  return async (dispatch) => {
-    const { data: created } = await axios.post('/api/products', product);
-    dispatch(_createProduct(created));
-    history.push('/products')
-  }
-}
-
-
-export const deleteProduct = (id, history) => {
-  return async (dispatch) => {
-    const { data: product } = await axios.delete(`/api/products/${id}`);
-    dispatch(_deleteProduct(product));
-    history.push('/products')
-  }
-}
-
-export const setProducts = (products) => ({
+export const _setProducts = (products) => ({
   type: SET_PRODUCTS,
   products,
 });
 
-export const fetchProducts = () => async (dispatch) => {
-  const { data } = await axios.get("/api/products");
-  dispatch(setProducts(data));
+const _createProduct = (product) => {
+  return {
+    type: CREATE_PRODUCT,
+    product,
+  };
 };
 
+const _deleteProduct = (product) => {
+  return {
+    type: DELETE_PRODUCT,
+    product,
+  };
+};
+
+// const _fetchProductType = (product) => {
+//   return {
+//     type: FETCH_PRODUCT_TYPE,
+//     product,
+//   };
+// };
+
+// THUNK CREATORS
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get('/api/products');
+      dispatch(_setProducts(data));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
+export const createProduct = (product, history) => {
+  return async (dispatch) => {
+    try {
+      const { data: created } = await axios.post('/api/products', product);
+      dispatch(_createProduct(created));
+      history.push('/products');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
+export const deleteProduct = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const { data: product } = await axios.delete(`/api/products/${id}`);
+      dispatch(_deleteProduct(product));
+      history.push('/products');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
+// export const fetchProductType = (categoryName) => {
+//   return async (dispatch) => {
+//     try {
+//       const { data } = await axios.get(
+//         `/api/products/category/${categoryName}`
+//       );
+//       dispatch(_fetchProductType(data));
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+// };
 
 const initialState = [];
 
@@ -59,11 +91,13 @@ export default function productsReducer(state = initialState, action) {
     case SET_PRODUCTS:
       return action.products;
     case CREATE_PRODUCT:
-      return [...state, action.product]
+      return [...state, action.product];
     // case UPDATE_PRODUCT:
     //   return action.campus
     case DELETE_PRODUCT:
       return state.filter((product) => product.id !== action.product.id);
+    // case FETCH_PRODUCT_TYPE:
+    //   return action.product;
     default:
       return state;
   }
