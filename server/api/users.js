cbabb846a4ec3c300d6d8ce2b192020973146fb9
nickyjhong <nicolehong95@gmail.com>
2022-08-07 15:01:37 -0400

    changed users API so we don't use /:id

--- server/api/users.js
+++ server/api/users.js
@@ -4,13 +4,14 @@ const {
 } = require('../db');
 module.exports = router;
 
+// ADMIN VIEW: RETRIEVE ALL USERS ***requireToken + isAdmin 
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the name and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['name', 'email'],
+      attributes: ['firstName', 'lastName', 'email'],
     });
     res.json(users);
   } catch (err) {
@@ -18,6 +19,7 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// ADMIN VIEW: CREATE NEW USER
 router.post('/', async (req, res, next) => {
   try {
     const newUser = await User.create(req.body);
@@ -28,124 +30,77 @@ router.post('/', async (req, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
+// ADMIN VIEW: UPDATE USER ***requireToken
+router.put('/:id', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id);
+    await user.update(req.body)
+    res.send(user)
+  } catch (error) {
+    next(error);
+  }
+});
+
+// ADMIN VIEW: DELETE USER
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+      await user.destroy();
+      res.send(user)
+  } catch (err) {
+    next(err);
+  }
+});
+
+// USER CAN SEE THEIR PROFILE -> NEED ***requireToken TO WORK
+router.get('/profile', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.dataValues.id, {
+      attributes: [
+        'name',
+        'email',
+      ],
+    });
     res.send(user);
   } catch (err) {
     next(err);
   }
 });
 
-// SHOWS ALL ORDERS
-router.get('/:id/orders', async (req, res, next) => {
+// USER CAN UPDATE THEIR PROFILE *TIER 2
+router.put('/account', async (req, res, next) => {
   try {
-    const userOrder = await User.findByPk(req.params.id, {
-      include: [
-        {
-          model: Order,
-          // where: {
-          //   status: 'open'
-          // }
-          include: [Product]
-        },
+    const user = await User.findByPk(req.user.dataValues.id, {
+      attributes: [
+        'firstName',
+        'lastName',
+        'address',
       ],
     });
-    res.send(userOrder);
-  } catch (err) {
-    next(err);
+    await user.update(req.body)
+    res.send(user)
+  } catch (error) {
+    next(error);
   }
 });
 
-// SHOWS ONLY ACTIVE ORDER ( AKA CART )
-router.get('/:id/cart', async (req, res, next) => {
+// USER CAN VIEW THEIR PAST ORDERS -> ***need requireToken to work
+router.get('/orders', async (req, res, next) => {
   try {
-    const userOrder = await User.findByPk(req.params.id, {
-      include: [
-        {
-          model: Order,
-          where: {
-            status: 'open',
+    const userOrder = await Order.findAll({
+      where: {
+        userId: req.user.dataValues.id,
+        status: 'closed'
+      },
+        include: [
+          {
+            model: Product,
           },
-          include: [Product]
-        },
-      ],
-      // attributes: ['email'],
-    });
+        ]
+      }
+    );
     res.send(userOrder);
   } catch (err) {
     next(err);
   }
 });
-
-router.get('/:id/orders/:orderId', async (req, res, next) => {
-  try {
-    const userOrder = await User.findByPk(req.params.id, {
-      include: [
-        {
-          model: Order,
-          where: {
-            id: req.params.orderId,
-          },
-        },
-      ],
-      attributes: ['email'],
-    });
-    res.send(userOrder);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/:id/cart', async (req, res, next) => {
-  try {
-    const userOrder = await User.findByPk(req.params.id, {
-      include: [
-        {
-          model: Order,
-        },
-      ],
-      attributes: ['email'],
-    });
-    res.send(userOrder);
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Once user and cart associated by id, come back to write code
-// router.put('/:id/cart', async (req, res, next) => {
-//   try {
-//     const userOrder = await User.findByPk(req.params.id, {
-//       include: [
-//         {
-//           model: Cart,
-//         },
-//       ],
-//       attributes: ['email'],
-//     });
-//     res.send(userOrder);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-// Update user, do this when we get to tier 2
-// router.put('/:id', async (req, res, next) => {
-//   try {
-//     const user = await User.findByPk(req.params.id);
-//     res.send(user);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// Create login token
-// router.post('/', async (req, res, next) => {
-//   try {
-//     const token = await User.authenticate(req.body);
-//     res.send(token);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
