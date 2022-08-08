fe8d9b8e0ad72f575b560042990e2fa549ffa7ff
nickyjhong <nicolehong95@gmail.com>
2022-08-08 11:28:59 -0400

    refactored cart GET route

--- server/api/cart.js
+++ server/api/cart.js
@@ -5,30 +5,26 @@ const {
 module.exports = router
 const { requireToken } = require('./middleware');
 
+// SEE CART
+
 router.get("/", requireToken, async (req, res, next) => {
   try {
     let order = await Order.findOne({
-        where: {
-            userId: req.user.dataValues.id,
-            status: 'open'
-        }
+      where: {
+        userId: req.user.dataValues.id,
+        status: 'open'
+      },
+      include: [Product]
     })
+    
     if (!order) {
-        order = await Order.create({
-            status: 'open',
-            userId: req.user.dataValues.id
-        })
+      order = await Order.create({
+        status: 'open',
+        userId: req.user.dataValues.id
+      })
     }
 
-    res.send(
-      await Order.findOne({
-        where:{
-          id: order.id
-        },
-        include:[Product],
-        order:[[Product, 'id','desc']] // adding order by so the result will not jump around
-      })
-    );
+    res.send(order);
   } catch (ex) {
     next(ex);
   }
