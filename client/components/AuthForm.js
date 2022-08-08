a39e035bee6caf3ea30011bf313e51ef08432a7e
Chris Tomshack <ctomshack15@gmail.com>
2022-08-08 12:51:55 -0400

    CSS: /login styling complete

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {authenticate} from '../store'
+import React from "react";
+import { connect } from "react-redux";
+import { authenticate } from "../store";
 
 /**
  * COMPONENT
  */
-const AuthForm = props => {
-  const {name, displayName, handleSubmit, error} = props
+const AuthForm = (props) => {
+  const { name, displayName, handleSubmit, error } = props;
 
   return (
     <div className="form">
       <form onSubmit={handleSubmit} name={name}>
-        <div className="form-container">
+        <div class="login">
+          <div>
+            <p class="title">Log In</p>
+            <input name="email" placeholder="Email" required />
+            <input name="password" placeholder="Password" type="password" required />
+            <button class="loader">Sign in</button>
+            <a href="#">Forgot your password?</a>
+            <p class="text">Don"t have an account?</p>
+            <button class="buttonShadow" type="submit">Create new account</button>
+          </div>
+        </div>
+        {/* <div className="form-container">
           <div className="input-container">
             <label htmlFor="email">
               <p>Email</p>
@@ -27,12 +38,12 @@ const AuthForm = props => {
           <div>
             <button type="submit">{displayName}</button>
           </div>
-        </div>
+        </div> */}
         {error && error.response && <div> {error.response.data} </div>}
       </form>
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
@@ -41,13 +52,13 @@ const AuthForm = props => {
  *   function, and share the same Component. This is a good example of how we
  *   can stay DRY with interfaces that are very similar to each other!
  */
-const mapLogin = state => {
+const mapLogin = (state) => {
   return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.auth.error
-  }
-}
+    name: "login",
+    displayName: "Login",
+    error: state.auth.error,
+  };
+};
 
 // const mapSignup = state => {
 //   return {
@@ -57,17 +68,17 @@ const mapLogin = state => {
 //   }
 // }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const email = evt.target.email.value
-      const password = evt.target.password.value
-      dispatch(authenticate(email, password, formName))
-    }
-  }
-}
+      evt.preventDefault();
+      const formName = evt.target.name;
+      const email = evt.target.email.value;
+      const password = evt.target.password.value;
+      dispatch(authenticate(email, password, formName));
+    },
+  };
+};
 
-export const Login = connect(mapLogin, mapDispatch)(AuthForm)
+export const Login = connect(mapLogin, mapDispatch)(AuthForm);
 // export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
