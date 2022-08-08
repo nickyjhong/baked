eaf72265508e1b27812013616c31bfd95367177e
nickyjhong <nicolehong95@gmail.com>
2022-08-08 16:35:56 -0400

    cart POST

--- server/api/cart.js
+++ server/api/cart.js
@@ -7,50 +7,49 @@ const { requireToken } = require('./middleware');
 
 // SEE CART
 
-// router.get('/', requireToken, async (req, res, next) => {
-//   try {
-//     let order = await Order.findOne({
-//       where: {
-//         userId: req.user.dataValues.id,
-//         status: 'open',
-//       },
-//       include: [Product],
-//     });
-//     res.send(order);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-router.get('/:userId', async(req, res, next) => {
+router.get('/', requireToken, async (req, res, next) => {
   try {
-    let cart = await Order.findOne({
+    let order = await Order.findOne({
       where: {
-        userId: req.params.userId
-      }
-    })
-  } catch(err) {
-    next(err)
+        userId: req.user.dataValues.id,
+        status: 'open',
+      },
+      include: [Product],
+    });
+    res.send(order);
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-// router.post('/:userId', async (req, res, send) => {
+// router.get('/:userId', async(req, res, next) => {
 //   try {
-//     let order = await Order.findOne({
+//     let cart = await Order.findOne({
 //       where: {
-//         // userId: req.user.dataValues.id,
-//         userId: req.params.userId,
-//         status: 'open',
-//       },
-//       include: [Product, CartItem],
-//     });
+//         userId: req.params.userId
+//       }
+//     })
+//   } catch(err) {
+//     next(err)
+//   }
+// })
 
-//     if (!order) {
-//       order = await Order.create({
-//         status: 'open',
-//         userId: req.params.userId,
-//       });
-//     }
+router.post('/', async (req, res, send) => {
+  try {
+    let order = await Order.findOne({
+      where: {
+        userId: req.user.dataValues.id,
+        status: 'open',
+      },
+      include: [Product, CartItem],
+    });
+
+    if (!order) {
+      order = await Order.create({
+        status: 'open',
+        userId: req.user.dataValues.id,
+      });
+    }
 
     // Look in cartItems to find product by orderId and productId
     // If it doesn't exist 
@@ -62,11 +61,28 @@ router.get('/:userId', async(req, res, next) => {
         // orderId
     // return whole order (whats in cart)
 
-//     res.send(order);
-//   } catch (err) {
-//     next(err);
-//   }
-// })
+    let product = await CartItem.findOne({
+      where: {
+        orderId: order.id,
+        productId: req.body.productId
+      }
+    })
+
+    if (!product) {
+      await CartItem.create({
+        orderId: order.id,
+        productId: req.body.productId
+      })
+    } else {
+      product.update({
+        quantity: 2
+      })
+    }
+    res.send(order);
+  } catch (err) {
+    next(err);
+  }
+})
 
 // FRONT END:
   // Button -> add product (using id)
@@ -74,11 +90,3 @@ router.get('/:userId', async(req, res, next) => {
 
 // BACK END:
   // API Route to find order that is open and add product to cart
-
-router.post('/', async (req, res, next) => {
-  try {
-    let cart = await Order.create(req.body.userId)
-  } catch(err) {
-    next(err)
-  }
-})
