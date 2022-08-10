eb776c913c43d5741f84ce924db7f7050beb6196
nickyjhong <nicolehong95@gmail.com>
2022-08-10 17:45:14 -0400

    added search to admin view

--- client/components/AdminShop.js
+++ client/components/AdminShop.js
@@ -9,6 +9,7 @@ export class AdminShop extends Component {
     super();
     this.state = {
       filtered: 'All',
+      term: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -18,7 +19,7 @@ export class AdminShop extends Component {
   }
 
   handleChange(event) {
-    this.setState({ filtered: event.target.value });
+    this.setState({ ...this.state, term: event.target.value });
   }
 
   handleFilterClick(categoryName) {
@@ -26,14 +27,14 @@ export class AdminShop extends Component {
   }
 
   render() {
+    console.log('TERM', this.state.term)
     const { products } = this.props;
+
     const productFilter = products.filter((product) => {
       if (this.state.filtered === 'All') return product;
       if (this.state.filtered === 'Cakes') return product.category === 'cake';
-      if (this.state.filtered === 'Cookies')
-        return product.category === 'cookie';
-      if (this.state.filtered === 'Cupcakes')
-        return product.category === 'cupcake';
+      if (this.state.filtered === 'Cookies') return product.category === 'cookie';
+      if (this.state.filtered === 'Cupcakes') return product.category === 'cupcake';
       if (this.state.filtered === 'Mini') return product.category === 'mini';
       if (this.state.filtered === 'Other') return product.category === 'other';
     });
@@ -43,7 +44,12 @@ export class AdminShop extends Component {
         Baked Goods
         <div className="grid-container">
           <div className="grid-section-left">
-            <input className="category-search" placeholder="Search"></input>
+            <input 
+              className="category-search" 
+              placeholder="Search"
+              onChange={this.handleChange}
+            >
+            </input>
             <p className="grid-section-left-category-bold">Categories</p>
             <ul>
               <li
@@ -97,31 +103,34 @@ export class AdminShop extends Component {
               </li>
             </ul>
           </div>
+
           <div className="grid-section-right">
-            {productFilter.map((product) => {
-              const displayPrice = parseFloat(product.price / 100).toFixed(2) 
-              return (
+            {productFilter.map((product) => (
+              product.name.toLowerCase().includes(this.state.term.toLowerCase()) ? (
                 <div className="grid-item" key={product.id}>
-                  <Link to={`/products/${product.id}`} >
-                    <img
-                      className="shop-image"
-                      src={product.imageURL}
-                      alt={`Image of ${product.name}`}
-                    />
-                  </Link>
-                  <div className="cookie-description">
-                    <h3 className="grid-item-text">{product.name}</h3>
-                    <p className="grid-item-text">${displayPrice}</p>
-                    <div className="shop-btn-container">
-                      <Link to={`/products/${product.id}/update`}>
-                        <button className='view-more-btn shop-btn'>Edit Product</button>
-                      </Link>
-                      <button className='add-to-cart shop-btn' onClick={() => {this.props.addToCart(product); alert('added to cart') }}>Add To Cart</button>
-                    </div>
+                <Link to={`/products/${product.id}`} >
+                  <img
+                    className="shop-image"
+                    src={product.imageURL}
+                    alt={`Image of ${product.name}`}
+                  />
+                </Link>
+                <div className="cookie-description">
+                  <h3 className="grid-item-text">{product.name}</h3>
+                  <p className="grid-item-text">${parseFloat(product.price / 100).toFixed(2)}</p>
+                  <div className="shop-btn-container">
+                    <Link to={`/products/${product.id}/update`}>
+                      <button className='view-more-btn shop-btn'>Edit Product</button>
+                    </Link>
+                    <button className='add-to-cart shop-btn' onClick={() => {this.props.addToCart(product); alert('added to cart') }}>Add To Cart</button>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+                ) : (
+                  ""
+                )
+              ))
+            }
           </div>
         </div>
       </section>
@@ -139,3 +148,5 @@ const mapDispatchToProps = (dispatch, { history }) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AdminShop);
+
+
