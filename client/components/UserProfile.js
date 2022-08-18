199d1d7db21cbfd58d93e19c1681d5ac03e8031e
nickyjhong <nicolehong95@gmail.com>
2022-08-18 13:38:21 -0400

    added edit profile button on profile page

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../store/singleUser';
 import OrderHistory from './OrderHistory';
+import { Link } from "react-router-dom";
 
 export class UserProfile extends React.Component {
   constructor(props) {
@@ -16,7 +17,13 @@ export class UserProfile extends React.Component {
     return (
       <div className="profile-container">
         <h2 className="profile-heading">Welcome {this.props.firstName}!</h2>
-
+        <div className="profile-btn-container">
+          <Link to="/profile/update">
+            <button type="button" className="update-profile">
+              Update profile
+            </button>
+          </Link>
+        </div>
         <div className="profile-order-container">
           <OrderHistory />
         </div>
